d383519e33c9b0f2ccb39a17bbfba579b8935712
changbaol <changbaol@tujia.com>
2019-10-02 18:04:47 +0800

    2019年10月02日18:04:46

--- src/main/java/com/lcb/jdbc/TestJDBC.java
+++ src/main/java/com/lcb/jdbc/TestJDBC.java
@@ -7,7 +7,8 @@ import java.sql.*;
  * @version $
  */
 public class TestJDBC {
-    static{
+
+    static {
         try {
             // 1、加载驱动
             Class.forName(Driver.class.getName());
@@ -16,19 +17,47 @@ public class TestJDBC {
         }
     }
 
-    public static void main(String[] args) throws Exception{
-        Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test_db?useSSL=false&characterEncoding=utf-8", "root", "Li13613766084");
-        PreparedStatement preparedStatement = connection.prepareStatement("select * from Tickets64 where id = ?");
-        preparedStatement.setInt(1,1);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        while(resultSet.next()){
-            String columnName1 = resultSet.getMetaData().getColumnName(1);
-            String columnName2 = resultSet.getMetaData().getColumnName(2);
-            System.out.println(columnName1 + ":" + resultSet.getInt(1));
-            System.out.println(columnName2 + ":" + resultSet.getString(2));
+    public static void main(String[] args) throws Exception {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DriverManager
+                .getConnection("jdbc:mysql://127.0.0.1:3306/test_db?useSSL=false&characterEncoding=utf-8", "root",
+                               "Li13613766084");
+            preparedStatement = connection.prepareStatement("select * from Tickets64 where id = ?");
+            preparedStatement.setInt(1, 1);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String columnName1 = resultSet.getMetaData().getColumnName(1);
+                String columnName2 = resultSet.getMetaData().getColumnName(2);
+                System.out.println(columnName1 + ":" + resultSet.getInt(1));
+                System.out.println(columnName2 + ":" + resultSet.getString(2));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
     }
 }
